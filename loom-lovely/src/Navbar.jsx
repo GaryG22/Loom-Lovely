d4e4436e22f05703590a6eea43ed30720287db6a
gary gritsch <garygritsch@gmail.com>
2023-12-11 10:48:21 -0800

    added navbar to top of home page

---
+++ loom-lovely/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function Navbar(){
+    return (
+        <ul>
+            <li><a href="#new">New</a></li>
+            <li><a href="#chuncky">Chuncky Bags</a></li>
+            <li><a href="#fluffy">Fluffy Bags</a></li>
+            <li><a href="#mini"> Mini Bags</a></li>
+            <li><a href="#shop"> Shop All</a></li>
+            <li><a href="#contact"> Contact Us!</a></li>
+        </ul>
+    )
+}
+
+export default Navbar
