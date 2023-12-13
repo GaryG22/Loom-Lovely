408cd97823a1d81179fb35fa6460950f73435218
gary gritsch <garygritsch@gmail.com>
2023-12-12 18:18:04 -0800

    added section 2 as well as new font and clouds to top of section 2

--- loom-lovely/src/Home.jsx
+++ loom-lovely/src/Home.jsx
@@ -5,12 +5,10 @@ function Home(){
     return (
 
         <div className = "home-container">
-            <h1 className = "home-title">Made By Hand With Love</h1>
+            <h1 className = "home-title">Today is a Beautiful Day!</h1>
             <p className = "home-p">this will be dummy text for the description and grabber of the bag. shoppers will have a general undertsanding of what is being sold.</p>
-            <div className = "bag-container">
-
-            </div>
         </div>
+        
     )
 }
 
---
+++ loom-lovely/src/Home2.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function Section1(){
+
+    return(
+
+        <div className = "section1">
+            <img className = "mountians" src = "https://i.postimg.cc/Y0BXyncm/cloudssssss-Photo-Room.png"/>
+
+        </div>
+    )
+}
+
+export default Section1
